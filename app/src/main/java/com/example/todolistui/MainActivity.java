6598f311043d5930fb27ddc331bc129cd1a89de0
irfans18 <Nforspeed521@gmail.com>
2020-09-24 10:42:20 +0700

    tugas2

--- app/src/main/java/com/example/todolistui/MainActivity.java
+++ app/src/main/java/com/example/todolistui/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.todolistui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lvItem = findViewById(R.id.listAct);
+
+        for(int i=0; i<10; i++){
+            list.add("Kegiatan " + i);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        lvItem.setAdapter(adapter);
     }
 }
